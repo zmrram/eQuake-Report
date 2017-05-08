359b176747594c4c1fe582744174e23fcd80765b
Tin Huynh <Tin Huynh>
2017-05-07 21:33:04 -0700

    create ggmap.js to handle all ggmap functions

--- js/ajax_request.js
+++ js/ajax_request.js
@@ -1,5 +1,5 @@
 $(function() {
-    function worldDataAJAX(mag) {
+    function worldDataAJAX() {
         var yesterday = new Date(Date.now() - 864e5).toISOString().slice(0, 10);
         $.ajax({
             url: 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson',
@@ -9,105 +9,16 @@ $(function() {
                 'starttime': yesterday
             },
             success: function(response) {
-                if (mag) {
-                    filterData(response, mag);
-                } else {
-                    $.each(response.features, function(index, val) {
-                        addMarkers(val);
-                    });
-                }
+                $.each(response.features, function(index, val) {
+                    addMarkers(val);
+                });
             }
         })
     }
 
-    function filterData(data, mag) {
-        switch (mag) {
-            case 1:
-                $.each(data.features, function(index, val) {
-                    if (val.properties.mag < 2.5) {
-                        addMarkers(val);
-                    }
-                });
-                break;
-            case 2:
-                $.each(data.features, function(index, val) {
-                    if (val.properties.mag >= 2.5 && val.properties.mag < 5.5) {
-                        addMarkers(val);
-                    }
-                });
-                break;
-            case 3:
-                $.each(data.features, function(index, val) {
-                    if (val.properties.mag >= 5.5 && val.properties.mag < 6.1) {
-                        addMarkers(val);
-                    }
-                });
-                break;
-            case 4:
-                $.each(data.features, function(index, val) {
-                    if (val.properties.mag >= 6.1 && val.properties.mag < 7) {
-                        addMarkers(val);
-                    }
-                });
-                break;
-            case 5:
-                $.each(data.features, function(index, val) {
-                    if (val.properties.mag >= 7) {
-                        addMarkers(val);
-                    }
-                });
-                break;
-        }
-    }
-
-    function addInfo(data) {
-        var content = "";
-        content += "Location: " + data.properties.place + "<br>";
-        content += "Magnitude: " + data.properties.mag + "<br>";
-        content += "Time: " + new Date(data.properties.time);
-        return content;
-    }
-
-    function chooseMarker(magnitude) {
-        marker = "";
-        if (magnitude < 2.5) {
-            marker = 'http://labs.google.com/ridefinder/images/mm_20_white.png';
-        } else if (magnitude >= 2.5 && magnitude < 5.5) {
-            marker = 'http://labs.google.com/ridefinder/images/mm_20_blue.png';
-        } else if (magnitude >= 5.5 && magnitude < 6.1) {
-            marker = 'http://labs.google.com/ridefinder/images/mm_20_green.png';
-        } else if (magnitude >= 6.1 && magnitude < 6.9) {
-            marker = 'http://labs.google.com/ridefinder/images/mm_20_orange.png';
-        } else {
-            marker = 'http://labs.google.com/ridefinder/images/mm_20_red.png';
-        }
-        return marker;
-    }
-
-    function removeMarkers() {
-        for (i = 0; i < allMarkers.length; i++) {
-            allMarkers[i].setMap(null);
-        }
-    }
-
     function addMarkers(data) {
-        var lng = data.geometry.coordinates[0];
-        var lat = data.geometry.coordinates[1];
-        var latlng = lat + "," + lng;
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lat, lng),
-            animation: google.maps.Animation.DROP,
-            icon: chooseMarker(data.properties.mag),
-            map: map
-        });
+        var marker = ggmap.addMarker(data);
         allMarkers.push(marker);
-        var content = addInfo(data);
-        google.maps.event.addListener(marker, 'click', (function(marker) {
-            return function() {
-                infoWindow.setContent(content);
-                infoWindow.open(map, marker);
-            }
-        })(marker));
         // $.ajax({
         //     url: 'https://maps.googleapis.com/maps/api/geocode/json?',
         //     type: 'GET',
@@ -138,14 +49,13 @@ $(function() {
                 userCoord = position.coords;
                 var yesterday = new Date(Date.now() - 864e5).toISOString().slice(0, 10);
                 var requestParam = {
-                	'starttime' : yesterday,
+                    'starttime': yesterday,
                     'latitude': userCoord.latitude,
                     'longitude': userCoord.longitude,
                     'maxradiuskm': radius
                 };
                 selectedDataAJAX(requestParam);
             }
-
             navigator.geolocation.getCurrentPosition(success, error);
         } else {
             alert('Geolocation is not supported')
@@ -168,80 +78,57 @@ $(function() {
 
     }
 
-    function mapInit() {
-        var mapOptions = {
-            zoom: 5,
-            minZoom: 2,
-            center: new google.maps.LatLng(32.09024, -100, 712991),
-            panControl: false,
-            panControlOptions: {
-                position: google.maps.ControlPosition.BOTTOM_LEFT
-            },
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.LARGE,
-                position: google.maps.ControlPosition.RIGHT_CENTER
-            },
-            scaleControl: false
-        };
-
-        infoWindow = new google.maps.InfoWindow({
-            content: ""
+    function getForm() {
+        $('#radius-box').slideToggle('slow', function() {
+            $('#submit').click(function(e) {
+                e.preventDefault();
+                $('#radius-box').slideUp('slow');
+                ggmap.removeMarkers();
+                var radius = $('#search-radius').val();
+                if (radius === "") {
+                    getUserCoord('160');
+                } else {
+                    radius = (parseInt(radius) * 0.621371).toString();
+                    getUserCoord(radius);
+                }
+            });
         });
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-        google.maps.event.addListener(map, 'click', function() {
-            infoWindow.close();
-        });
-        return map;
-    }
-
-    function getForm(){
-    	$('#radius-box').slideToggle('slow', function() {
-    		$('#submit').click(function(e){
-    			e.preventDefault();
-    			$('#radius-box').slideUp('slow');
-    			removeMarkers();
-    			var radius = $('#search-radius').val();
-    			if (radius === ""){
-    				getUserCoord('160');
-    			}
-    			else {
-    				radius = (parseInt(radius) * 0.621371).toString();
-    				getUserCoord(radius);
-    			}
-    		});
-    	});
     }
 
     function listener() {
         $('#allEQ').click(function() {
-            removeMarkers();
+            ggmap.removeMarkers();
             worldDataAJAX();
         });
         $('#xsEQ').click(function() {
-            removeMarkers();
-            worldDataAJAX(1);
+        	var icon = $('#xsEQ').children()[0].currentSrc;
+        	ggmap.filterMap(icon);
         });
         $('#sEQ').click(function() {
-            removeMarkers();
-            worldDataAJAX(2);
+        	var icon = $('#sEQ').children()[0].currentSrc;
+        	ggmap.filterMap(icon);
         });
         $('#mEQ').click(function() {
-            removeMarkers();
-            worldDataAJAX(3);
+        	var icon = $('#mEQ').children()[0].currentSrc;
+        	ggmap.filterMap(icon);
         });
         $('#lEQ').click(function() {
-            removeMarkers();
-            worldDataAJAX(4);
+        	var icon = $('#lEQ').children()[0].currentSrc;
+        	ggmap.filterMap(icon);
         });
         $('#xlEQ').click(function() {
-            removeMarkers();
-            worldDataAJAX(5);
+        	var icon = $('#xlEQ').children()[0].currentSrc;
+        	ggmap.filterMap(icon);
         });
         $('#myEQ').click(getForm);
+        $('#filterEQ').click(function() {
+            $('#filter-buttons').slideToggle('slow', function() {
+
+            });
+        });
     }
     var allMarkers = [];
-    var map = mapInit();
+    var map = ggmap.mapInit();
     listener();
 
 });
---
+++ js/ggmap.js
@@ -0,0 +1,100 @@
+var ggmap = (function() {
+    var map = null;
+    var allMarkers = [];
+
+    function addInfo(data) {
+        var content = "";
+        content += "Location: " + data.properties.place + "<br>";
+        content += "Magnitude: " + data.properties.mag + "<br>";
+        content += "Time: " + new Date(data.properties.time);
+        return content;
+    }
+
+    function chooseMarker(magnitude) {
+        marker = "";
+        if (magnitude < 2.5) {
+            marker = 'http://labs.google.com/ridefinder/images/mm_20_white.png';
+        } else if (magnitude >= 2.5 && magnitude < 5.5) {
+            marker = 'http://labs.google.com/ridefinder/images/mm_20_blue.png';
+        } else if (magnitude >= 5.5 && magnitude < 6.1) {
+            marker = 'http://labs.google.com/ridefinder/images/mm_20_green.png';
+        } else if (magnitude >= 6.1 && magnitude < 6.9) {
+            marker = 'http://labs.google.com/ridefinder/images/mm_20_orange.png';
+        } else {
+            marker = 'http://labs.google.com/ridefinder/images/mm_20_red.png';
+        }
+        return marker;
+    }
+
+    function clearMarkers() {
+        for (i = 0; i < allMarkers.length; i++) {
+            allMarkers[i].setMap(null);
+        }
+    }
+
+    return {
+        mapInit: function() {
+            var mapOptions = {
+                zoom: 5,
+                minZoom: 2,
+                center: new google.maps.LatLng(32.09024, -100, 712991),
+                panControl: false,
+                panControlOptions: {
+                    position: google.maps.ControlPosition.BOTTOM_LEFT
+                },
+                zoomControl: true,
+                zoomControlOptions: {
+                    style: google.maps.ZoomControlStyle.LARGE,
+                    position: google.maps.ControlPosition.RIGHT_CENTER
+                },
+                scaleControl: false
+            };
+
+            infoWindow = new google.maps.InfoWindow({
+                content: ""
+            });
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            google.maps.event.addListener(map, 'click', function() {
+                infoWindow.close();
+            });
+            return map;
+        },
+        addMarker: function(data) {
+            var lng = data.geometry.coordinates[0];
+            var lat = data.geometry.coordinates[1];
+            var latlng = lat + "," + lng;
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(lat, lng),
+                animation: google.maps.Animation.DROP,
+                icon: chooseMarker(data.properties.mag),
+                map: map
+            });
+            var content = addInfo(data);
+            google.maps.event.addListener(marker, 'click', (function(marker) {
+                return function() {
+                    infoWindow.setContent(content);
+                    infoWindow.open(map, marker);
+                }
+            })(marker));
+            allMarkers.push(marker);
+            return marker;
+        },
+
+        removeMarkers: function() {
+            for (i = 0; i < allMarkers.length; i++) {
+                allMarkers[i].setMap(null);
+            }
+            allMarkers = [];
+        },
+
+        filterMap: function(icon) {
+            clearMarkers();
+            for (var i = 0; i < allMarkers.length; i++) {
+                if (allMarkers[i].icon === icon){
+                	allMarkers[i].setAnimation(google.maps.Animation.BOUNCE);
+                	allMarkers[i].setMap(map);
+                }
+            }
+        }
+    };
+})();
