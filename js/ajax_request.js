959ec8710df6780dcabca426b6bc2cdc6f266ecc
Tin Huynh <Tin Huynh>
2017-05-11 16:42:18 -0700

    search earthquakes

--- js/ajax_request.js
+++ js/ajax_request.js
@@ -16,24 +16,68 @@ $(function() {
         })
     }
 
-    function addMarkers(data) {
-        // $.ajax({
-        //     url: 'https://maps.googleapis.com/maps/api/geocode/json?',
-        //     type: 'GET',
-        //     dataType: 'json',
-        //     data: {
-        //         'latlng': latlng,
-        //         'key': 'AIzaSyDvwaz-AlpoWn3GEa2xPDhdnmZ5eZaq--g'
-        //     },
-        //     success: function(response) {
-        //         if (response.results.length !== 0){
-        //         	console.log(response.results[response.results.length - 1].formatted_address);
-        //         }
-        //         else{
-        //         	console.log("Ocean");
-        //         }
-        //     }
-        // })
+    function collectInput() {
+        if ($('#address').val() != "") {
+            $.ajax({
+                url: 'https://maps.googleapis.com/maps/api/geocode/json?',
+                type: 'GET',
+                dataType: 'json',
+                data: {
+                    'address': $('#address').val(),
+                    'key': 'AIzaSyDvwaz-AlpoWn3GEa2xPDhdnmZ5eZaq--g'
+                },
+                success: function(response) {
+                    var range = "";
+                    if (response.status !== "ZERO_RESULTS") {
+                        if (response.results[0].types.includes('premise') ||
+                            response.results[0].types.includes('route') ||
+                            response.results[0].types.includes('street address') ||
+                            response.results[0].types.includes('postal_code')) {
+                            if ($('#range').val() == '') {
+                                $('#range-box').slideDown('slow');
+                            } else {
+                                var location = response.results[0].geometry.location;
+                                var query = {
+                                'starttime' : $('#starttime').val(),
+                                'endtime': $('#endtime').val(),
+                                'latitude': location.lat,
+                                'longitude': location.lng,
+                                'maxradiuskm': $('#range').val()
+                                };
+                                selectedDataAJAX(query);
+                            }
+                        }
+                        else{
+                            var bounds = response.results[0].geometry.bounds;
+                            var minlatitude = bounds.southwest.lat;
+                            var minlongitude = bounds.southwest.lng;
+                            var maxlatitude = bounds.northeast.lat;
+                            var maxlongitude = bounds.northeast.lng;
+                            if (parseFloat(minlatitude) > parseFloat(maxlatitude)){
+                                minlatitude = (parseFloat(minlatitude) * -1).toString();
+                                maxlatitude = (parseFloat(maxlatitude) * -1).toString();
+                            }
+                            if (parseFloat(minlongitude) > parseFloat(maxlongitude)){
+                                minlongitude = (parseFloat(minlongitude) * -1).toString();
+                                maxlongitude = (parseFloat(maxlongitude) * -1).toString();
+                            }
+
+                            var query = {
+                                'starttime' : $('#starttime').val(),
+                                'endtime': $('#endtime').val(),
+                                'minlatitude' : minlatitude,
+                                'minlongitude' : minlongitude,
+                                'maxlatitude' : maxlatitude,
+                                'maxlongitude' : maxlongitude
+                            };
+                            selectedDataAJAX(query);
+                        }
+                    }
+                }
+            })
+        } else {
+            alert("Empty Address");
+        }
     }
 
     function getUserCoord(radius) {
@@ -45,7 +89,7 @@ $(function() {
 
             function success(position) {
                 userCoord = position.coords;
-                 map.setCenter(new google.maps.LatLng(userCoord.latitude, userCoord.longitude));
+                map.setCenter(new google.maps.LatLng(userCoord.latitude, userCoord.longitude));
                 var yesterday = new Date(Date.now() - 864e5).toISOString().slice(0, 10);
                 var requestParam = {
                     'starttime': yesterday,
@@ -101,36 +145,38 @@ $(function() {
             worldDataAJAX();
         });
         $('#xsEQ').click(function() {
-        	var icon = $('#xsEQ').children()[0].currentSrc;
-        	ggmap.filterMap(icon);
+            var icon = $('#xsEQ').children()[0].currentSrc;
+            ggmap.filterMap(icon);
         });
         $('#sEQ').click(function() {
-        	var icon = $('#sEQ').children()[0].currentSrc;
-        	ggmap.filterMap(icon);
+            var icon = $('#sEQ').children()[0].currentSrc;
+            ggmap.filterMap(icon);
         });
         $('#mEQ').click(function() {
-        	var icon = $('#mEQ').children()[0].currentSrc;
-        	ggmap.filterMap(icon);
+            var icon = $('#mEQ').children()[0].currentSrc;
+            ggmap.filterMap(icon);
         });
         $('#lEQ').click(function() {
-        	var icon = $('#lEQ').children()[0].currentSrc;
-        	ggmap.filterMap(icon);
+            var icon = $('#lEQ').children()[0].currentSrc;
+            ggmap.filterMap(icon);
         });
         $('#xlEQ').click(function() {
-        	var icon = $('#xlEQ').children()[0].currentSrc;
-        	ggmap.filterMap(icon);
+            var icon = $('#xlEQ').children()[0].currentSrc;
+            ggmap.filterMap(icon);
         });
         $('#cancel-filter').click(function() {
             var icon = $('#cancel-filter').children()[0].currentSrc;
             ggmap.filterMap(icon);
         });
 
-        $('#myEQ').click(getForm);
-        $('#filterEQ').click(function() {
-            $('#filter-buttons').slideToggle('slow', function() {
-
-            });
+        $('#searchEQ').click(function(event) {
+            event.preventDefault();
+            $('#range-box').slideUp('slow');
+            collectInput();
         });
+        $('#myEQ').click(getForm);
+
+
     }
     var map = ggmap.mapInit();
     listener();
